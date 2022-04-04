043e0bc1f239bd4368687fce7fbe26d2d8265efc
admin <hypermail@yandex.ru>
2022-04-04 20:31:04 +0300

    vk on windows

--- _vk_db_creation.sql
+++ _vk_db_creation.sql
@@ -143,7 +143,36 @@ CREATE TABLE `photos` (
     FOREIGN KEY (media_id) REFERENCES media(id)
 );
 
-SHOW TABLES;
+DROP TABLE IF EXISTS higher_education;
+CREATE TABLE higher_education (
+	id SERIAL,
+	user_id BIGINT UNSIGNED NOT NULL,
+	institute VARCHAR(255) COMMENT 'Институт',
+	faculty VARCHAR(255) COMMENT 'Факультет',
+	education_form VARCHAR(255) COMMENT 'Форма обучения',
+	status VARCHAR(255) COMMENT 'Текущий статус учащегося',
+	
+	FOREIGN KEY (user_id) REFERENCES users(id)
+) COMMENT 'Высшее образование';
+
+DROP TABLE IF EXISTS secondary_education;
+CREATE TABLE secondary_education (
+	id SERIAL,
+	user_id BIGINT UNSIGNED NOT NULL,
+	school VARCHAR(255) COMMENT 'Школа',
+	year_end VARCHAR(255) COMMENT 'Год окончания',
+	
+	FOREIGN KEY (user_id) REFERENCES users(id)
+) COMMENT 'Среднее образование';
+
+DROP TABLE IF EXISTS wall;
+CREATE TABLE wall(
+	user_id BIGINT UNSIGNED NOT NULL,
+	media_id BIGINT UNSIGNED NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (media_id) REFERENCES media(id)
+)COMMENT 'Стена';
 
 ALTER TABLE vk.likes 
 ADD CONSTRAINT likes_fk 
